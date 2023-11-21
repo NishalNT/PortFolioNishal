60cbbb0bada68c3a9ed31c25d7826b034d76d8eb
NishalNT <nishalnthingalaya1@gmail.com>
2023-11-21 10:40:03 +0530

    dog

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ ScrollReveal({
     reset: true,
     distance: '80px',
     duration: 2000,
-    delay: 200
+    delay: 100
  });
 
  ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
